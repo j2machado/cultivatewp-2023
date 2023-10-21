361ccd80f8585ce5fe7ea1c8281de592e81950c0
Obi Juan <hola@obijuan.dev>
2023-10-21 14:12:08 -0500

    Add footer logo to the footer and the primary color as the footer's background color.

--- wordpress/wp-content/themes/JuanJoseMachado-2023/inc/site-footer.php
+++ wordpress/wp-content/themes/JuanJoseMachado-2023/inc/site-footer.php
@@ -15,6 +15,7 @@ use BEStarter\Blocks\Social_Links;
  * Site Footer
  */
 function be_site_footer() {
+	echo '<a href="' . esc_url( home_url() ) . '" rel="home" class="site-header__logo" aria-label="' . esc_attr( get_bloginfo( 'name' ) ) . ' Home"> <img src="'. get_stylesheet_directory_uri() .'/assets/logos/logo-footer.svg" alt="'. get_bloginfo( 'name' ) .'"></a>';
 	echo '<p>&copy;' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '. All rights reserved.</p>';
 }
 add_action( 'tha_footer_top', 'be_site_footer' );
